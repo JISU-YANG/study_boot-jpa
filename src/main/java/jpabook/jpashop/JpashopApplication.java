d6392aeb89d33b4fdf34fc6a5201d568c8ec3187
JISU-YANG <jisuyang@egoist.im>
2021-05-17 11:01:25 +0900

    [ADD] JPA Repository, JUnit Test
    JUnit을 이용해 JPA Reposiory를 테스트했습니다.
    셋팅정보다 xml에서 yml으로 변경됐습니다.

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+		/*
+ 		TODO 1. Lombok 테스트
+		Hello hello = new Hello();
+		hello.setData("hello");
+		String data = hello.getData();
+		System.out.println("data = " + data);
+		*/
+
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
